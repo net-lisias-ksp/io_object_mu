4db83eef3ecfcbf952c8bf6d143a1692e2e5646c
DeertaV <160445485+DeertaV@users.noreply.github.com>
2026-03-29 00:17:15 +0800

    Delete test-import.py

--- test-import.py
+++
@@ -1,9 +0,0 @@
-import bpy
-import sys
-from io_object_mu.import_mu import import_mu
-
-collection = bpy.context.layer_collection.collection
-base = sys.argv.index("--")
-argv = sys.argv[base:]
-print(sys.argv)
-import_mu(collection, argv[1], True, False, False)
